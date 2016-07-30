38d4c93715d728196280d07106a4d167dbcabf34
Han Sooloo <hansooloo@gmail.com>
2016-07-30 11:43:31 -0400

    * tab/whitespace cleanup.

--- update_plex.sh
+++ update_plex.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-/mnt/PMS.backup/PMS_Updater/PMS_Updater.sh		\
-			-c /root/plex_auth		\
-			-a				\
-			-v				
+#
+# Assumes PMS related mount being in place.
+#
+
+/mnt/PMS.backup/PMS_Updater/PMS_Updater.sh   \
+			-c /root/plex_auth               \
+			-a                               \
+			-v
